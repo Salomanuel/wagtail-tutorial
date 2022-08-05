05c7c5d4a040f0a2a26baff32115abd0e0c7c201
Salomanuel <manuel.salomone@finstreet.de>
2022-08-05 12:24:30 +0200

    feat(global-site-settings): add media links in footer

--- site_settings/models.py
+++ site_settings/models.py
@@ -1,23 +1,31 @@
+from django.db import models
 
+from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.admin.edit_handlers import FieldPanel
 
-from modelcluster.models import models
-
-
+@register_setting
 class SocialMediaSettings(BaseSetting):
 
     facebook = models.URLField(
         blank=True,
-        help_text='Enter your ____ URL',
+        help_text='Enter your Facebook URL',
     )
     twitter = models.URLField(
         blank=True,
-        help_text='Enter your ____ URL',
+        help_text='Enter your Twitter URL',
     )
     youtube = models.URLField(
         blank=True,
-        help_text='Enter your ____ URL',
+        help_text='Enter your Youtube URL',
     )
     instagram = models.URLField(
         blank=True,
-        help_text='Enter your ____ URL',
+        help_text='Enter your Instagram URL',
     )
+
+    panels = [
+        FieldPanel("facebook"),
+        FieldPanel("twitter"),
+        FieldPanel("youtube"),
+        FieldPanel("instagram"),
+    ]
