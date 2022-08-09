7274f1868b68015665e8ebfa4365e68ccc5b37a2
Salomanuel <manuel.salomone@finstreet.de>
2022-08-09 13:06:52 +0200

    feat(global-site-settings): finish lesson

--- site_settings/models.py
+++ site_settings/models.py
@@ -2,6 +2,31 @@ from django.db import models
 
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+
+@register_setting
+class HoursSettings(BaseSetting):
+    hours = RichTextField(
+            blank=True,
+            null=True,
+            features=[],
+    )
+
+    panels = [
+        FieldPanel("hours")
+    ]
+@register_setting
+
+class ContactSettings(BaseSetting):
+    contact = RichTextField(
+            blank=True,
+            null=True,
+            features=["link"]
+    )
+
+    panels = [
+        FieldPanel("contact")
+    ]
 
 @register_setting
 class SocialMediaSettings(BaseSetting):
